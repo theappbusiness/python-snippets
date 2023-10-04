6863ed6b0a9cfb00183abc143da39ff8216509f2
Chris Topping <chris.topping@kinandcarta.com>
2023-10-04 12:36:48 +0100

    removed hardcoded function list and getting them dynamically

--- dynamically-populate-function-args.py
+++ dynamically-populate-function-args.py
@@ -1,9 +1,6 @@
 '''
 This is an example of how to dynamically extract the required arguments of functions and pass the relevant variable as needed.
 Create any number of functions and a number of variables. As long as the variable names match the names of the function arguments this will work.
-
-Improvement: how can we programmatically extract the list of functions to call and then call them one by one?
-function_list = ([f.__name__ for f in globals().values() if type(f) == type(lambda *args: None)]) - this produces a list of functions with each one being a string, which is not callable.
 '''
 
 import inspect
@@ -27,13 +24,26 @@ country = 'UK'
 # list of variables in the current context using vars()
 vars_dict = {k: v for k, v in vars().items()}
 
-# List of functions
-functions = [myfunc1, myfunc2]
+# Get the current module (the module where the script is running)
+current_module = inspect.currentframe().f_globals
 
-# Loop through functions and dynamically pass arguments
-for function in functions:
-    arg_names = inspect.getfullargspec(function).args
-    args_to_pass = {arg_name: vars_dict[arg_name] for arg_name in arg_names if arg_name in vars_dict}
-    print(f'function: {function.__name__} -> function args: {arg_names} -> args_to_pass: {args_to_pass}')
-    # Call the function with the arguments
-    result = function(**args_to_pass)
+# Filter and extract only functions
+functions = [item for item in current_module.items() if inspect.isfunction(item[1])]
+
+# Loop through the list of functions and call each one
+for name, function in functions:
+    if name != 'main':  # Exclude the 'main' function if it exists
+        print(f"Calling function: {name}")
+        try:
+            # get the function arguments that are required
+            arg_names = inspect.getfullargspec(function).args
+
+            # get the variables to pass as function arguments
+            args_to_pass = {arg_name: vars_dict[arg_name] for arg_name in arg_names if arg_name in vars_dict}
+
+            # Call the function
+            result = function(**args_to_pass)
+            print(f"Function {name} returned: {result}")
+
+        except Exception as e:
+            print(f"Error calling function {name}: {e}")
